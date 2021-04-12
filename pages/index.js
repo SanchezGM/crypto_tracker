1d7b8bd051fd4ee5d58b9d8accfffca8108367c7
FrankJoseph <frankjoseph.serdenia.530@my.csun.edu>
2021-04-11 20:13:55 -0700

    Change to 20 per page

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home({filteredCoins}) {
 export const getServerSideProps = async () => {
   
   /* The API key  */
-  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false') 
+  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=25&page=1&sparkline=false') 
  
   const filteredCoins = await res.json()
 
