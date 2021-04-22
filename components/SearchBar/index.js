8ff498f551a5a08b08f9036bd0bb56f18ce426d8
jokyon <joseph.olympia.710@my.csun.edu>
2021-04-21 21:34:59 -0700

    add date

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -1,16 +1,22 @@
 
+import React from 'react';
 import styles from './Search.module.css';
+
+
+      
+let [month, date, year]    = new Date().toLocaleDateString("en-US").split("/");
 
 const SearchBar = ({...rest}) => {
     return (
         <div className= {styles.coin_subheader}>
             <div className= {styles.coin_search}>
-                <h1>Top 25 Crytocurrencies - *Add Date Here*</h1>
+          
+                <h1>Top 25 Crytocurrencies - { month + '/' + date + "/" + year } </h1>
+               
             </div>
             <div className= {styles.coin_search}>
                 <input class={styles.coin_input}{...rest}/>                          
-            </div>
-            
+            </div>            
         </div>
     )
 }
